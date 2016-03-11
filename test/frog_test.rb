fa8cabc966dbfca0a872ad2c09ec78a8b6f18125
Dionysis Athinaios <d.athinaios@gmail.com>
2016-03-11 02:07:15 +0000

    create test draft

--- test/frog_test.rb
+++ test/frog_test.rb
@@ -1,11 +1,30 @@
 require 'test_helper'
 
 class FrogTest < Minitest::Test
+
+  def setup
+    capture_io{Frog::Interface.start(['switch', 'frog_test'])}.join ''
+    @data = FrogConfig.read_todo_file('frog_test')
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Frog::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # def test_add
+  #   out = capture_io{Frog::Interface.start(['add', 'this was added by test'])}.join ''
+  #   assert_match /Added succesfully to:/, out
+  #   assert_match @data['TODO'].last, 'This was added by test'
+  # end
+
+  # def test_remove
+  #   last_addition_index = @data['TODO'].count - 1
+  #   out = capture_io{Frog::Interface.start(['remove', last_addition_index])}.join ''
+  # end
+
+  # any_instance_of(Frog::Interface) do |cli|
+  #   mock(cli).yes?(anything){ true }
+  # end
+
 end
+
